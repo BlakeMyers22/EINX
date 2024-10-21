cd7cf0390dee5b99e0cc403d96b70e5f1e9e106e
BlakeMyers22 <167836117+BlakeMyers22@users.noreply.github.com>
2024-10-21 18:44:06 -0500

    Update RealEstateTokenization.sol

--- RealEstateTokenization.sol
+++ RealEstateTokenization.sol
@@ -1,31 +1,68 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract RealEstateTokenization {
+// Import ERC721URIStorage for handling token metadata
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/access/AccessControl.sol";
+
+contract RealEstateTokenization is ERC721URIStorage, AccessControl {
+    uint256 public tokenCounter;
+
     struct Property {
         uint256 tokenId;
         string propertyDetails;
         uint256 valuation;
-        string[] mediaUrls;  // Store IPFS URLs for media
     }
 
     mapping(uint256 => Property) public properties;
-    uint256 public tokenCounter;
 
-    constructor() {
-        tokenCounter = 1;
+    // Define the MINTER_ROLE
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    // Event for property tokenization
+    event PropertyTokenized(uint256 tokenId, string propertyDetails, uint256 valuation, string tokenURI);
+
+    constructor(address[] memory minters) ERC721("Estate Industries Real Estate", "EIRE") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+        tokenCounter = 0;
+
+        // Grant MINTER_ROLE to specified addresses
+        for (uint256 i = 0; i < minters.length; i++) {
+            _setupRole(MINTER_ROLE, minters[i]);
+        }
     }
 
-    event PropertyTokenized(uint256 tokenId, string propertyDetails, uint256 valuation, string[] mediaUrls);
-
-    function tokenizeProperty(string memory _propertyDetails, uint256 _valuation, string[] memory _mediaUrls) public {
-        uint256 tokenId = tokenCounter;
-        properties[tokenId] = Property(tokenId, _propertyDetails, _valuation, _mediaUrls);
-        emit PropertyTokenized(tokenId, _propertyDetails, _valuation, _mediaUrls);
-        tokenCounter++;
+    // Override supportsInterface to resolve the conflict
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
+    // Tokenize a property and set tokenURI
+    function tokenizeProperty(
+        string memory _propertyDetails, 
+        uint256 _valuation, 
+        string memory _tokenURI  // Accept the tokenURI as a parameter
+    ) public onlyRole(MINTER_ROLE) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, _tokenURI);  // Set the token URI
+
+        Property memory newProperty = Property({
+            tokenId: newTokenId,
+            propertyDetails: _propertyDetails,
+            valuation: _valuation
+        });
+
+        properties[newTokenId] = newProperty;
+        tokenCounter += 1;
+
+        emit PropertyTokenized(newTokenId, _propertyDetails, _valuation, _tokenURI);
+    }
+
+    // Fetch property details based on token ID
     function getProperty(uint256 _tokenId) public view returns (Property memory) {
+        require(_exists(_tokenId), "Property does not exist");
         return properties[_tokenId];
     }
 }
