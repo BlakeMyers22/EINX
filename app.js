f3246fccb3f01e93e23ff07740a8fbe83065c9b7
BlakeMyers22 <167836117+BlakeMyers22@users.noreply.github.com>
2024-10-21 15:33:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,548 +3,83 @@
 let web3;
 let contract;
 let accounts;
-let contractOwnerAddress = '0x1A56608C8FcbdfA771878bA7126339CB66330580'; // Replace with your contract owner's address
+
+const contractAddress = '0x24c1A7F0bCA70C780b776643E218cf2Bd98955d3';
 const contractABI = [
-	{
-		"inputs": [],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "owner",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "approved",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "uint256",
-				"name": "tokenId",
-				"type": "uint256"
-			}
-		],
-		"name": "Approval",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "owner",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "operator",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "bool",
-				"name": "approved",
-				"type": "bool"
-			}
-		],
-		"name": "ApprovalForAll",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "previousOwner",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "newOwner",
-				"type": "address"
-			}
-		],
-		"name": "OwnershipTransferred",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "tokenId",
-				"type": "uint256"
-			},
-			{
-				"indexed": false,
-				"internalType": "string",
-				"name": "propertyDetails",
-				"type": "string"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "valuation",
-				"type": "uint256"
-			}
-		],
-		"name": "PropertyTokenized",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "from",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "uint256",
-				"name": "tokenId",
-				"type": "uint256"
-			}
-		],
-		"name": "Transfer",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "tokenId",
-				"type": "uint256"
-			}
-		],
-		"name": "approve",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "owner",
-				"type": "address"
-			}
-		],
-		"name": "balanceOf",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "tokenId",
-				"type": "uint256"
-			}
-		],
-		"name": "getApproved",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_tokenId",
-				"type": "uint256"
-			}
-		],
-		"name": "getProperty",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "uint256",
-						"name": "tokenId",
-						"type": "uint256"
-					},
-					{
-						"internalType": "string",
-						"name": "propertyDetails",
-						"type": "string"
-					},
-					{
-						"internalType": "uint256",
-						"name": "valuation",
-						"type": "uint256"
-					}
-				],
-				"internalType": "struct RealEstateTokenization.Property",
-				"name": "",
-				"type": "tuple"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "owner",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "operator",
-				"type": "address"
-			}
-		],
-		"name": "isApprovedForAll",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "name",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "owner",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "tokenId",
-				"type": "uint256"
-			}
-		],
-		"name": "ownerOf",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "properties",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "tokenId",
-				"type": "uint256"
-			},
-			{
-				"internalType": "string",
-				"name": "propertyDetails",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "valuation",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "renounceOwnership",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "from",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "tokenId",
-				"type": "uint256"
-			}
-		],
-		"name": "safeTransferFrom",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "from",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "tokenId",
-				"type": "uint256"
-			},
-			{
-				"internalType": "bytes",
-				"name": "data",
-				"type": "bytes"
-			}
-		],
-		"name": "safeTransferFrom",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "operator",
-				"type": "address"
-			},
-			{
-				"internalType": "bool",
-				"name": "approved",
-				"type": "bool"
-			}
-		],
-		"name": "setApprovalForAll",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "bytes4",
-				"name": "interfaceId",
-				"type": "bytes4"
-			}
-		],
-		"name": "supportsInterface",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "symbol",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "tokenCounter",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "tokenId",
-				"type": "uint256"
-			}
-		],
-		"name": "tokenURI",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_propertyDetails",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_valuation",
-				"type": "uint256"
-			}
-		],
-		"name": "tokenizeProperty",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "from",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "tokenId",
-				"type": "uint256"
-			}
-		],
-		"name": "transferFrom",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "newOwner",
-				"type": "address"
-			}
-		],
-		"name": "transferOwnership",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
+    {
+        "inputs": [
+            {
+                "internalType": "address[]",
+                "name": "minters",
+                "type": "address[]"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    // ... (Rest of your ABI as provided)
+    {
+        "inputs": [],
+        "name": "tokenURI",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
 ];
-const contractAddress = '0x2b3138F8196eb7f0e619E1CD715da6590b033a34'; // Replace with your contract's address
 
 window.addEventListener('load', async () => {
     if (typeof window.ethereum !== 'undefined') {
         web3 = new Web3(window.ethereum);
 
         // Request account access
-        await ethereum.request({ method: 'eth_requestAccounts' });
-        accounts = await web3.eth.getAccounts();
+        try {
+            await ethereum.request({ method: 'eth_requestAccounts' });
+            accounts = await web3.eth.getAccounts();
 
-        // Update UI with connected wallet address
-        document.getElementById('walletAddress').innerText = `Connected Wallet: ${accounts[0]}`;
+            // Update UI with connected wallet address
+            document.getElementById('walletAddress').innerText = `Connected Wallet: ${accounts[0]}`;
 
-        // Create a contract instance
-        contract = new web3.eth.Contract(contractABI, contractAddress);
+            // Create a contract instance
+            contract = new web3.eth.Contract(contractABI, contractAddress);
 
-        // Display sections based on the connected account
-        document.getElementById('getPropertySection').style.display = 'block';
+            // Display sections based on the connected account's role
+            document.getElementById('getPropertySection').style.display = 'block';
 
-        if (accounts[0].toLowerCase() === contractOwnerAddress.toLowerCase()) {
-            document.getElementById('tokenizePropertySection').style.display = 'block';
+            // Check if the connected account has MINTER_ROLE
+            checkMinterRole();
+
+            // Add event listeners
+            document.getElementById('tokenizePropertyButton').addEventListener('click', tokenizeProperty);
+            document.getElementById('getPropertyButton').addEventListener('click', getProperty);
+
+        } catch (error) {
+            console.error(error);
+            alert('User denied account access or an error occurred.');
         }
-
-        // Add event listeners
-        document.getElementById('tokenizePropertyButton').addEventListener('click', tokenizeProperty);
-        document.getElementById('getPropertyButton').addEventListener('click', getProperty);
 
     } else {
         alert('Please install MetaMask to use this DApp!');
     }
 });
+
+// Function to check if the user has MINTER_ROLE
+async function checkMinterRole() {
+    const MINTER_ROLE = web3.utils.keccak256('MINTER_ROLE');
+    try {
+        const hasRole = await contract.methods.hasRole(MINTER_ROLE, accounts[0]).call();
+        if (hasRole) {
+            document.getElementById('tokenizePropertySection').style.display = 'block';
+        }
+    } catch (error) {
+        console.error('Error checking minter role:', error);
+    }
+}
 
 // Function to tokenize a new property
 async function tokenizeProperty() {
@@ -560,7 +95,7 @@ async function tokenizeProperty() {
         await contract.methods.tokenizeProperty(propertyDetails, valuation).send({ from: accounts[0] });
         alert('Property tokenized successfully!');
     } catch (error) {
-        console.error(error);
+        console.error('Error tokenizing property:', error);
         alert('Error tokenizing property.');
     }
 }
@@ -581,7 +116,7 @@ async function getProperty() {
             <p><strong>Valuation:</strong> ${property.valuation}</p>
         `;
     } catch (error) {
-        console.error(error);
+        console.error('Error fetching property details:', error);
         alert('Error fetching property details.');
     }
 }
@@ -593,7 +128,7 @@ document.getElementById('connectWalletButton').addEventListener('click', async (
             await ethereum.request({ method: 'eth_requestAccounts' });
             window.location.reload();
         } catch (error) {
-            console.error(error);
+            console.error('Error connecting wallet:', error);
             alert('Error connecting wallet.');
         }
     } else {
