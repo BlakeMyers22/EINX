945a240bbcfaf1a6b7733e9ac7927010e175550c
BlakeMyers22 <167836117+BlakeMyers22@users.noreply.github.com>
2024-10-21 14:55:00 -0500

    Update RealEstateTokenization.sol

--- RealEstateTokenization.sol
+++ RealEstateTokenization.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Import statements
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract RealEstateTokenization is ERC721, Ownable {
+contract RealEstateTokenization is ERC721, AccessControl {
     uint256 public tokenCounter;
 
     struct Property {
@@ -15,13 +16,24 @@ contract RealEstateTokenization is ERC721, Ownable {
 
     mapping(uint256 => Property) public properties;
 
+    // Define the MINTER_ROLE
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    // Event for property tokenization
     event PropertyTokenized(uint256 tokenId, string propertyDetails, uint256 valuation);
 
-    constructor() ERC721("Estate Industries Real Estate", "EIRE") {
+    constructor(address[] memory minters) ERC721("Estate Industries Real Estate", "EIRE") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
         tokenCounter = 0;
+
+        // Grant MINTER_ROLE to specified addresses
+        for (uint256 i = 0; i < minters.length; i++) {
+            _setupRole(MINTER_ROLE, minters[i]);
+        }
     }
 
-    function tokenizeProperty(string memory _propertyDetails, uint256 _valuation) public onlyOwner {
+    function tokenizeProperty(string memory _propertyDetails, uint256 _valuation) public onlyRole(MINTER_ROLE) {
         uint256 newTokenId = tokenCounter;
         _safeMint(msg.sender, newTokenId);
 
